7a57cd118686e55ec8dc7813e21aa7c59c5204f4
ramyabingi <135826098+ramyabingi@users.noreply.github.com>
2023-06-07 23:53:28 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace exam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            int i, j;
+            int n = 5;
+            for(i=1;i<=n;i++)
+            {
+                for(j=1;j<=i;j++)
+                {
+                    Console.Write((char)(i + 64));
+                }
+                Console.WriteLine("");
+            }
+            Console.ReadLine();
+
+            string s1 = "Ramya";
+            string rev = " ";
+            char[] ch1 = s1.ToCharArray();
+            for (int k = ch1.Length - 1; k>= 0; k--)
+            {
+                rev = rev + ch1[i];
+            }
+            Console.Write(rev);
+            Console.WriteLine();
+
+        }
+
+    }
+}
---
+++ SendingEmails.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Net.Mail;
+
+namespace SendingEmail
+{
+    public partial class SendingEmails : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void sbnbtn_Click(object sender, EventArgs e)
+        {
+            MailMessage mail = new MailMessage();
+            mail.To.Add(email.Text.ToString().Trim());
+            mail.From = new MailAddress("bingir411@gmail.com","ramyabingi01@gmail.com");
+            mail.Subject = "From Amazon";
+            mail.Body = "<p>hello Srikanth Adepu<br>How are you..?";
+            mail.IsBodyHtml = true;
+            SmtpClient smtp = new SmtpClient();
+            smtp.Port = 587;
+            smtp.EnableSsl =true ;
+            smtp.UseDefaultCredentials =false ;
+            smtp.Host = "smtp.gmail.com";
+            smtp.Credentials = new System.Net.NetworkCredential("bingir411@gmail.com", "imidjyywrhhvsgya");
+            smtp.Send(mail);
+
+        }
+    }
+}
