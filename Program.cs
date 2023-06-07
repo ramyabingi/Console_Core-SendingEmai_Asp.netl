bc17288ad1676485206b4975c5b3ea3e0daff697
ramyabingi <135826098+ramyabingi@users.noreply.github.com>
2023-06-08 00:01:25 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,35 +1,72 @@
 ﻿using System;
 
-namespace exam
+namespace fibbonaci
 {
     class Program
     {
         static void Main(string[] args)
         {
-          
-            int i, j;
-            int n = 5;
-            for(i=1;i<=n;i++)
-            {
-                for(j=1;j<=i;j++)
-                {
-                    Console.Write((char)(i + 64));
-                }
-                Console.WriteLine("");
-            }
-            Console.ReadLine();
 
-            string s1 = "Ramya";
+            ///////polyndrome string////////////
+            string s = "Ramya";
             string rev = " ";
-            char[] ch1 = s1.ToCharArray();
-            for (int k = ch1.Length - 1; k>= 0; k--)
+            char[] ch = s.ToCharArray();
+            for (int i = ch.Length - 1; i >= 0; i--)
             {
-                rev = rev + ch1[i];
+                rev = rev + ch[i];
             }
             Console.Write(rev);
             Console.WriteLine();
 
-        }
 
+            string n1 = "RamyaSrikanth";
+            int count = 0;
+            for (int j = 0; j < n1.Length; j++)
+            {
+                count = count + 1;
+            }
+            Console.WriteLine(count);
+            Console.WriteLine();
+
+            //  Console.WriteLine("Hello World!");
+            int a = 0;
+            int b = 1;
+            int sum = 0;
+            for (int i = 1; i <= 10; i++)
+            {
+                a = b;
+                b = sum;
+                sum = a + b;
+                Console.WriteLine(sum);
+            }
+            Console.WriteLine();
+
+
+            int[] a1 = { 1, 2, 2, 4, 6, 6 };
+            
+            for (int e = 0; e <= a1.Length - 1; e++)
+            {
+                int c = 0;
+                for (int f = 1; f <= a1[e]; f++)
+                {
+                    if (e % f == 0)
+                    {
+                        c = c + 1;
+                    }
+
+                }
+                if (count == 2)
+                {
+                    Console.WriteLine(a1[e]);
+                }
+
+
+
+            }
+        }
     }
+
+
+
 }
+
